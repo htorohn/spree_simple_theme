bb88c2f56d6631b6d27f8960e2f24369ec4406fe
Hector Toro <htorohn@gmail.com>
2017-08-03 18:50:54 +0000

    corregida la validacion de locale es

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.precompile += %w( spree/frontend/template/*)
+Rails.application.config.assets.precompile += %w( jquery.validate/localization/messages_es.js )
 #Rails.application.config.assets.precompile += %w( spree/frontend/theme.js )
