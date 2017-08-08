5c8d1d77e6b6a8966d36dd84867d81c7cb6a8ae3
Hector Toro <htorohn@gmail.com>
2017-08-08 19:49:48 +0000

    added messages_esmx.js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.precompile += %w( spree/frontend/template/*)
 Rails.application.config.assets.precompile += %w( jquery.validate/localization/messages_es.js )
+Rails.application.config.assets.precompile += %w( jquery.validate/localization/messages_esmx.js )
 #Rails.application.config.assets.precompile += %w( spree/frontend/theme.js )
