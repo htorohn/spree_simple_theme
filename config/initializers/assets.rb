3507259029ff815b5cff64ca04a60d10f79970ca
Hector <htorohn@gmail.com>
2017-08-11 12:56:37 -0600

    select2.js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,5 @@
 Rails.application.config.assets.precompile += %w( spree/frontend/template/*)
+Rails.application.config.assets.precompile += %w( spree/backend/select2_locale_es-MX.js)
 Rails.application.config.assets.precompile += %w( jquery.validate/localization/* )
 #Rails.application.config.assets.precompile += %w( jquery.validate/localization/messages_es.js )
 #Rails.application.config.assets.precompile += %w( jquery.validate/localization/messages_esmx.js )
