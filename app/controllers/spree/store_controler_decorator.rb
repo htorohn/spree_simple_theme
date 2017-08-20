b77b63f44a8c8e2f86e188be50c1fef1535dae1e
Hector <htorohn@gmail.com>
2017-08-20 17:21:41 -0600

    comment best sellers

--- app/controllers/spree/store_controler_decorator.rb
+++ app/controllers/spree/store_controler_decorator.rb
@@ -17,7 +17,7 @@ class Spree::StoreController
 	def load_best_sellers
 	    # config = Spree::BestSellersConfiguration.new
 	    # if ((config.has_preference?(:show_best_sellers) && config[:show_best_sellers]) || (config.has_preference?(:show_best_sellers_sidebar) && config[:show_best_sellers_sidebar]))
-	      @best_sellers = (Spree::Product.best_sellers rescue nil)
+	    @best_sellers = (Spree::Product.best_sellers rescue nil)
 	    # end
 	end
 
